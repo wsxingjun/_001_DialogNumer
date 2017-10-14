a7a8eaebd82e2ce74dc3130fd893bdb95c1fdaf3
wsxingjun <1185889308@qq.com>
2017-10-14 19:58:43 +0800

    第二次提交：完成拨打电话的功能

--- app/src/main/java/com/example/administrator/a001_dialognum/MainActivity.java
+++ app/src/main/java/com/example/administrator/a001_dialognum/MainActivity.java
@@ -1,7 +1,12 @@
 package com.example.administrator.a001_dialognum;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +14,27 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        /*1.首先要找到EditText和Button控件*/
+        final EditText et_performNum = (EditText) findViewById(R.id.et_performNum);
+        final Button bt_dialogNum = (Button) findViewById(R.id.bt_dialogNum);
+
+
+
+        /* 2.设置Button的监听器*/
+        bt_dialogNum.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                 /* 3.获取EditText中的电话号码*/
+                String DialogNumer = et_performNum.getText().toString().trim();
+                /* 4.拨打电话*/
+                Intent Dialogintent = new Intent();
+                Dialogintent.setAction(Intent.ACTION_CALL);
+                //Dialogintent.setData(Uri.parse("tel:"+DialogNumer));
+                Dialogintent.setData(Uri.parse("tel:"+DialogNumer));
+                startActivity(Dialogintent);
+            }
+        });
+
     }
 }
